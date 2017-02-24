7019f356fb99a58a7fd3ec1aa4a760bf44621720
MasaruTech <masaru.tech@gmail.com>
2017-02-24 11:05:18 +0900

    お酒の名前 or 読みで検索できるようにした

--- app/controllers/api/sakes_controller.rb
+++ app/controllers/api/sakes_controller.rb
@@ -1,6 +1,12 @@
 class Api::SakesController < Api::ApplicationController
   def index
-    sakes = paginate Sake.all.order(:id), page: params[:page], per_page: params[:per_page] || 20
+    if params[:keyword].blank?
+      sakes = Sake.all
+    else
+      sakes = Sake.where('name like :keyword or yomi like :keyword', {keyword: "%#{params[:keyword]}%"})
+    end
+
+    sakes = paginate sakes.order(:id), page: params[:page], per_page: params[:per_page] || 20
 
     render json: sakes.map{|sake| Resources::Sake.new(sake).as_json(only: [:id, :name, :yomi])}
   end
