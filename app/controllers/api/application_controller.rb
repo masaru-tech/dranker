95f6f202b58986229c5f6f9fdd65e29a4440849d
MasaruTech <masaru.tech@gmail.com>
2017-04-06 22:46:14 +0900

    API処理時の予期せぬエラーの時にステータスコード500を返すようにした

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -4,5 +4,19 @@ class Api::ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
+  around_action :wrap_api
+
   respond_to :json
+
+  private
+    def wrap_api
+      begin
+        yield
+      rescue => ex
+        logger.error ex.message
+        logger.error ex.backtrace.join("\n")
+
+        head :internal_server_error
+      end
+    end
 end
